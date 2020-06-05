9cfa5bfbf18b292444f2265ea324dfdc305c134e
arynyestos <arynyestos@gmail.com>
2020-06-05 21:06:39 +0200

    Listeners de posición y velocidad.

---
+++ robominer_control/scripts/ft_listener.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import WrenchStamped
+import math
+import time
+
+#Seleccion de pata
+print 'Seleccione lado (r o l)'
+lado = raw_input()
+print 'Seleccione numero de pata'
+num = int(input())
+	
+def callback(data,args):
+	print args
+	print 'Fuerza:', math.sqrt(data.wrench.force.x**2 + data.wrench.force.y**2 + data.wrench.force.z**2), 'N'
+	print 'Par:', math.sqrt(data.wrench.torque.x**2 + data.wrench.torque.y**2 + data.wrench.torque.z**2), 'Nm'
+	time.sleep(2)
+	
+	    
+def listener():
+
+	# In ROS, nodes are uniquely named. If two nodes with the same
+	# name are launched, the previous one is kicked off. The
+	# anonymous=True flag means that rospy will choose a unique
+	# name for our 'listener' node so that multiple listeners can
+	# run simultaneously.
+	rospy.init_node('robominer_ft_sensor_listener', anonymous=True)
+	
+	
+	
+	if lado == 'r':
+		if num == 1:
+			rospy.Subscriber("/robominer/Rev3_r_1_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev3_r_1_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev5_r_1_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev5_r_1_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev8_r_1_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev8_r_1_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev11_r_1_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev11_r_1_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			
+	if lado == 'r':
+		if num == 2:
+			rospy.Subscriber("/robominer/Rev3_r_2_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev3_r_2_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev5_r_2_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev5_r_2_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev8_r_2_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev8_r_2_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev11_r_2_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev11_r_2_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+					
+	if lado == 'r':
+		if num == 3:
+			rospy.Subscriber("/robominer/Rev3_r_3_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev3_r_3_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev5_r_3_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev5_r_3_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev8_r_3_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev8_r_3_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev11_r_3_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev11_r_3_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			
+	if lado == 'l':
+		if num == 1:
+			rospy.Subscriber("/robominer/Rev3_l_1_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev3_l_1_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev5_l_1_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev5_l_1_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev8_l_1_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev8_l_1_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev11_l_1_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev11_l_1_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+							
+	if lado == 'l':
+		if num == 2:			
+			#Pata l2
+			rospy.Subscriber("/robominer/Rev3_l_2_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev3_l_2_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev5_l_2_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev5_l_2_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev8_l_2_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev8_l_2_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev11_l_2_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev11_l_2_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			
+	if lado == 'l':
+		if num == 3:
+			#Pata l3
+			rospy.Subscriber("/robominer/Rev3_l_3_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev3_l_3_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev5_l_3_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev5_l_3_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev8_l_3_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev8_l_3_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+			rospy.Subscriber("/robominer/Rev11_l_3_ft_sensor_topic", WrenchStamped, callback, "/robominer/Rev11_l_3_ft_sensor_topic", queue_size=1, buff_size=65536) 
+			time.sleep(.5)
+	
+
+	# spin() simply keeps python from exiting until this node is stopped
+	rospy.spin()
+
+if __name__ == '__main__':
+    listener()
---
+++ robominer_control/scripts/pos_listener.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import JointState
+#from control_msgs.msg import FollowJointTrajectoryActionResult
+#from control_msgs.msg import FollowJointTrajectoryActionGoal
+
+#Seleccion de pata
+print 'Seleccione lado (r o l)'
+lado = raw_input()
+print 'Seleccione numero de pata'
+num = int(input())
+
+def callback(data):
+	#Damos la orientacion de cada articulacion en orden del cuerpo al pie
+	if lado == 'r':
+		if num == 1:
+			print "Pos r1: (", data.position[9], data.position[15], data.position[21], data.position[3], ")"
+	if lado == 'r':
+		if num == 2:
+			print "Pos r2: (", data.position[10], data.position[16], data.position[22], data.position[4], ")"				
+	if lado == 'r':
+		if num == 3:
+			print "Pos r3: (", data.position[11], data.position[17], data.position[23], data.position[5], ")"
+	if lado == 'l':
+		if num == 1:
+			print "Pos l1: (", data.position[6], data.position[12], data.position[18], data.position[0], ")"				
+	if lado == 'l':
+		if num == 2:		
+			print "Pos l2: (", data.position[7], data.position[13], data.position[19], data.position[1], ")"
+	if lado == 'l':
+		if num == 3:
+			print "Pos l3: (", data.position[8], data.position[14], data.position[20], data.position[2], ")"
+	#rospy.sleep(2)
+
+def listener():
+	rospy.init_node('robominer_joint_position_subscriber', anonymous=True)
+	rospy.Subscriber("robominer/joint_states", JointState, callback, queue_size=1, buff_size=65536)   
+	rospy.spin()
+
+if __name__ == '__main__':
+    listener()
+
+
